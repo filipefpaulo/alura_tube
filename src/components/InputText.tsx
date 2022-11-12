c602af1673f99194c224a9c8992556e354894892
filipe <filipef.paulo@gmail.com>
2022-11-12 13:50:28 -0300

    InputText component

---
+++ src/components/InputText.tsx
@@ -0,0 +1,26 @@
+interface InputTextProps {
+  placeholder: string;
+  name: string;
+  value: string;
+  onChange: (evento: any) => void;
+  className?: string;
+}
+
+export function InputText({
+  placeholder,
+  name,
+  value,
+  onChange,
+  className,
+}: InputTextProps) {
+  return (
+    <input
+      className={`border border-zinc-400 p-3 px-4 bg-inherit text-inherit placeholder:text-inherit ${className}`}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+      type="text"
+      value={value}
+    />
+  );
+}
